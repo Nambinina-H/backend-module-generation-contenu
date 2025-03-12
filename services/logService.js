0ab68a66a3795e330e80a562dc4848a1eea7e48e
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2025-03-12 04:49:36 +0300

    feat : Ajout de la colonne "email" dans logs

--- services/logService.js
+++ services/logService.js
@@ -1,7 +1,18 @@
-const { supabase } = require('../services/supabaseService');
+const { supabaseAdmin } = require('../services/supabaseService');
 
 exports.logAction = async (userId, action, details = '') => {
-  await supabase
+  let email = "Email inconnu"; // Valeur par défaut si l'email n'est pas trouvé
+
+  // Récupérer l'email de l'utilisateur avec supabaseAdmin
+  if (userId) {
+    const { data: user, error } = await supabaseAdmin.auth.admin.getUserById(userId);
+    if (user && user.user && user.user.email) {
+      email = user.user.email;
+    }
+  }
+
+  // Insérer le log avec l'email
+  await supabaseAdmin
     .from('logs')
-    .insert([{ user_id: userId, action, details }]);
+    .insert([{ user_id: userId, email, action, details }]);
 };
