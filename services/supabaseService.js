ad2515f90fd21fc2b354be37ea0eb9341d159d45
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2025-03-22 11:12:40 +0300

    Revert "feat : -Add support for pagination and filters in the getLogs method."
    This reverts commit 604bc650bcd7a4bf34956dfc97aed26ad0cac88f.

--- services/supabaseService.js
+++ services/supabaseService.js
@@ -1,19 +1,11 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// ✅ Créer un client pour les actions normales
+// Créer un client classique pour les actions normales
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// ✅ Créer un client Admin pour les actions sensibles
+// Créer un client Admin avec SERVICE_ROLE_KEY pour les actions sensibles
 const supabaseAdmin = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY, {
   auth: { persistSession: false }
 });
-
-// ✅ Activer le mode Realtime pour les logs
-supabase
-  .channel('logs-channel')
-  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'logs' }, (payload) => {
-    console.log("🆕 Nouveau log ajouté :", payload.new);
-  })
-  .subscribe();
 
 module.exports = { supabase, supabaseAdmin };
