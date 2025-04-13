80b6e1c2292e44c347f6792e107f22ebc0734eb0
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2025-04-13 19:33:54 +0300

    feat: Update tweet log action to include tweet URL in the log message

--- controllers/oauth/twitterOAuthController.js
+++ controllers/oauth/twitterOAuthController.js
@@ -206,7 +206,7 @@ exports.publishTweet = async (req, res) => {
       tweetText: tweet.data.text
     });
     
-    await logAction(userId, 'twitter_publish', `Tweet publié : ${tweet.data.id}`);
+    await logAction(userId, 'twitter_publish', `Tweet publié : https://twitter.com/i/web/status/${tweet.data.id}`);
     console.log('📤 -------- Fin de la demande de publication d\'un tweet --------');
     
     res.json({
